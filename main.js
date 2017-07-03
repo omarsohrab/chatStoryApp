785059f8b95e02e050595551b6a25a572c240dbd
omarsohrab <omar.sohrab@gmail.com>
2017-07-04 00:06:20 +0600

    second commit

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 var chat_lines = document.getElementById('story-line');
 var nextbtn = document.getElementById('btn');
+var counter = 0;
 
 nextbtn.addEventListener("click",function(){
     var myRequest = new XMLHttpRequest();
-    myRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
+    myRequest.open('GET','http://www.json-generator.com/api/json/get/bUMQwiYAHS?indent=2');
     myRequest.onload = function(){
         var story = JSON.parse(myRequest.responseText);
-        getStoryLine();
+        getStoryLine(story,counter);
+        counter++;
     };
     myRequest.send();
 });
 
-function getStoryLine(){
-    chat_lines.insertAdjacentHTML('beforeend','sddyh');
+function getStoryLine(story,i){
+    var outputData = "";
+   
+    outputData +="<p>"+ "<div class=\"chip\">"+ story[i].name+"</div>"+"<br>"+story[i].about+"</p>"+"<hr>";
+        
+ 
+    chat_lines.insertAdjacentHTML('beforeend',outputData);
 }
